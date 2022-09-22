9cac74bb0c77eeb8122d2260694f2557a77ab22f
PinkLemon <paulgan98@gmail.com>
2022-09-22 09:25:53 -0700

    Initial commit of spring-kafka directory

---
+++ spring-kafka/src/main/java/com/springkafka/KafkaListeners.java
@@ -0,0 +1,22 @@
+package com.springkafka;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaListeners {
+
+    @KafkaListener(
+            topics = {"topic1", "topic2"},
+            groupId = "groupId")
+    void listener(String data) {
+        System.out.println("Listener received: " + data);
+    }
+
+//    @KafkaListener(
+//            topics = "topic1",
+//            groupId = "groupId")
+//    void listener1(String data) {
+//        System.out.println("Listener received: " + data);
+//    }
+}
