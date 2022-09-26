f8579d0b5c6466e688072132538cd84640ebf30c
PinkLemon <paulgan98@gmail.com>
2022-09-26 09:43:20 -0700

    Updated spring-kafka

--- spring-kafka/src/main/java/com/springkafka/KafkaListeners.java
+++ spring-kafka/src/main/java/com/springkafka/KafkaListeners.java
@@ -10,11 +10,13 @@ public class KafkaListeners {
             topics = {"topic1", "topic2"},
             groupId = "groupId")
     void listener(String data) {
+
         System.out.println("Listener received: " + data);
+
     }
 
 //    @KafkaListener(
-//            topics = "topic1",
+//            topics = {"topic1"},
 //            groupId = "groupId")
 //    void listener1(String data) {
 //        System.out.println("Listener received: " + data);
